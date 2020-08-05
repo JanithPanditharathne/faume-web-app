0cd32d1dc10fef3c2a7396f055a41070bc62aa7b
Leon De Silva <leond@zone24x7.com>
2020-08-05 08:54:00 +0530

    Modified request response logging filter to accept external correlation ids.

--- src/main/java/com/zone24x7/faume/webapp/accesslogs/RequestResponseLoggingFilter.java
+++ src/main/java/com/zone24x7/faume/webapp/accesslogs/RequestResponseLoggingFilter.java
@@ -1,5 +1,6 @@
 package com.zone24x7.faume.webapp.accesslogs;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -12,12 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.UUID;
 
+/**
+ * Class to represent the request response logging filter.
+ */
 @Component
 @Order(1)
 public class RequestResponseLoggingFilter implements Filter {
-
     private static final Logger LOGGER = LoggerFactory.getLogger("access");
 
+    private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
+    private static final String CORRELATION_ID_MDC_ATTRIBUTE_NAME = "correlationId";
+
+    /**
+     * Method to apply filtering.
+     *
+     * @param request the request
+     * @param response the response
+     * @param chain the filter chain
+     * @throws IOException if an IO error occurs
+     * @throws ServletException if an servlet error occurs
+     */
     @Override
     public void doFilter(
             ServletRequest request,
@@ -27,7 +42,7 @@ public class RequestResponseLoggingFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
-        String correlationId = UUID.randomUUID().toString();
+        final String correlationId = getCorrelationIdFromHeader(httpRequest);
 
         LOGGER.info("[REQUEST] UUID={} METHOD={} PATH={} QSTRING={} ORIGIN={}",
                     correlationId,
@@ -35,7 +50,7 @@ public class RequestResponseLoggingFilter implements Filter {
                     httpRequest.getServletPath(),
                     httpRequest.getQueryString(),
                     httpRequest.getRemoteAddr());
-        MDC.put("correlationId", "abc");
+        MDC.put(CORRELATION_ID_MDC_ATTRIBUTE_NAME, correlationId);
         long startTime = System.currentTimeMillis();
 
         chain.doFilter(request, response);
@@ -48,5 +63,23 @@ public class RequestResponseLoggingFilter implements Filter {
                     httpRequest.getRemoteAddr(),
                     System.currentTimeMillis() - startTime,
                     httpResponse.getStatus());
+
+        MDC.remove(CORRELATION_ID_MDC_ATTRIBUTE_NAME);
+    }
+
+    /**
+     * Method to get the correlation id from the header and generate if not available.
+     *
+     * @param request the request
+     * @return the correlation id
+     */
+    private String getCorrelationIdFromHeader(final HttpServletRequest request) {
+        String correlationId = request.getHeader(CORRELATION_ID_HEADER_NAME);
+
+        if (StringUtils.isEmpty(correlationId)) {
+            correlationId = UUID.randomUUID().toString();
+        }
+
+        return correlationId;
     }
 }
