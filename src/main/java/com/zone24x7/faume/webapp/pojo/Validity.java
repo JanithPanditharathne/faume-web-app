825ee32678a12e88c2703277d39ba19703554e87
Kanishka Wijesekara <kanishkamw@zone24x7.com>
2020-08-18 21:37:08 +0530

    added missing comments to Validity enum

--- src/main/java/com/zone24x7/faume/webapp/pojo/Validity.java
+++ src/main/java/com/zone24x7/faume/webapp/pojo/Validity.java
@@ -1,10 +1,18 @@
 package com.zone24x7.faume.webapp.pojo;
 
+/**
+ * Enum constants for validity status
+ */
 public enum Validity {
     VALID("VALID"),INVALID("INVALID");
 
     private String value;
 
+    /**
+     * Constructor to instantiate an enum constant
+     *
+     * @param value validity status value of the enum constant
+     */
     Validity(String value){
         this.value=value;
     }
